608e7718110442ac84150ff46119bcd0eba5ad6d
Firdaws Lamidi <lamidifirdaws@gmail.com>
2022-09-25 22:36:19 +0100

    settings

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -4,10 +4,10 @@ import SettingsForm from 'components/SettingsForm';
 export default function Dashboard() {
     return (
         <>
-        <div className="md:ml-64">
+        <div className="md:ml-64 mt-40">
            
 
-            <div className="px-3 md:px-8 h-auto -mt-24">
+            <div className="px-3 md:px-8 h-auto -mt-14">
                 <div className="container mx-auto max-w-full">
                     <div className="grid grid-cols-1 xl:grid-cols-6">
                         <div className="xl:col-start-1 xl:col-end-5 px-4 mb-16">
